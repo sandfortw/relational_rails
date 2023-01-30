4bb1ed47c95de11357df774883223a4437280c9f
Weston Sandfort <sandfortw@gmail.com>
2023-01-30 16:16:03 -0700

    Add a welcome page with a few links, and some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+  get '/authors', to: 'authors#index'
+  get '/authors/new', to: 'authors#new'
+  get '/books', to: 'books#index'
+  get '/books/new', to: 'books#new'
+
 end
