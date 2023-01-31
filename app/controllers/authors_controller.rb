9bbfb62ae31b40da7588877f1f5d1469819229ee
Weston Sandfort <sandfortw@gmail.com>
2023-01-30 20:10:15 -0700

    Add crud functionality for authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,6 +7,10 @@ class AuthorsController < ApplicationController
   def new
   end
 
+  def show
+    @author =Author.find(params[:id])
+  end
+
   def create
     author = Author.new({
       name: params[:author][:name],
@@ -16,6 +20,27 @@ class AuthorsController < ApplicationController
       updated_at: DateTime.now,
       })
     author.save
+    redirect_to '/authors'
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    author = Author.find(params[:id])
+    author.update({
+      name: params[:author][:name],
+      year_born: params[:author][:year_born].to_i,
+      dead_or_alive: ActiveModel::Type::Boolean.new.cast(params[:author][:living]),
+      updated_at: DateTime.now
+    })
+    author.save
+    redirect_to "/authors/#{author.id}"
+  end
+
+  def destroy
+    Author.destroy(params[:id])
     redirect_to '/authors'
   end
 end
