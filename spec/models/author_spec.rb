b084a99cb304202eb7519c5bc3e18949085a59f2
Weston Sandfort <sandfortw@gmail.com>
2023-01-31 19:37:38 -0700

    add tests for author model

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe Author do 
+  describe 'instance methods' do
+    describe '#name' do
+      it 'returns a name' do
+        author = Author.create!(name: 'Shakespeare', year_born: 1564, alive: false)
+        expect(author.name).to eq('Shakespeare')
+      end
+    end
+
+    describe '#year_born' do
+      it 'returns a year' do
+        author = Author.create!(name: 'Shakespeare', year_born: 1564, alive: false)
+        expect(author.year_born).to eq(1564)
+      end
+    end
+
+    describe '#alive?' do
+      it 'returns false if the author is dead' do
+        author = Author.create!(name: 'Shakespeare', year_born: 1564, alive: false)
+        expect(author.alive?).to eq(false)
+      end
+
+      it 'returns true if the author is alive' do
+        author = Author.create!(name: 'Malcolm Gladwell', year_born: 1963, alive: true)
+        expect(author.alive?).to eq(true)
+      end
+    end
+
+    describe '#created_at' do
+      it 'returns a datetime' do
+        author = Author.create!(name: 'Shakespeare', year_born: 1564, alive: false)
+        expect(author.created_at).to be_a(Time)
+      end
+    end
+
+    describe '#updated_at' do
+      it 'returns a datetime' do
+        author = Author.create!(name: 'Shakespeare', year_born: 1564, alive: false)
+        expect(author.updated_at).to be_a(Time)
+      end
+    end
+  end
+end
