35a06f8a3707b688e1e5f4213f14e592f39a608e
Weston Sandfort <sandfortw@gmail.com>
2023-01-31 20:07:29 -0700

    Add test #id for author and add instance tests for book

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -41,5 +41,19 @@ describe Author do
         expect(author.updated_at).to be_a(Time)
       end
     end
+
+    describe '#id' do
+      it 'returns an id as an integer' do
+        author = Author.create!(name: 'Shakespeare', year_born: 1564, alive: false)
+        expect(author.id).to be_an(Integer)
+      end
+
+      it 'increments by one' do
+        author = Author.create!(name: 'Shakespeare', year_born: 1564, alive: false)
+        expect(author.id).to be_an(Integer)
+        author2 = Author.create!(name: 'Malcolm Gladwell', year_born: 1963, alive: true)
+        expect(author2.id).to eq(author.id + 1)
+      end
+    end
   end
 end
