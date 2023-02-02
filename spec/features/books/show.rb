9cfb168a4e8379f53ab96786cf7e596690894e28
Weston Sandfort <sandfortw@gmail.com>
2023-02-01 19:55:29 -0700

    Add test for books show view and edited show view to add author id

--- spec/features/books/show.rb
+++ spec/features/books/show.rb
@@ -8,3 +8,18 @@
 # (data from each column that is on the child table)
 
 require 'rails_helper'
+
+describe 'books show' do
+  it 'should have the book with attributes' do
+    author = Author.create!(name: 'Malcolm Gladwell', year_born: 1963, alive: true)
+    book = Book.create!(title:'Outliers', author_id: author.id, year_written: 2008, fiction: false)
+    visit "/books/#{book.id}"
+    save_and_open_page
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(book.author_id)
+    expect(page).to have_content(book.year_written)
+    expect(page).to have_content(book.fiction)
+    expect(page).to have_content(book.created_at)
+    expect(page).to have_content(book.updated_at)
+  end
+end
