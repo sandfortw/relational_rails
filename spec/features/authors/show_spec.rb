d89dda96c532bc7b6a3f40d7bf310abb5943bf5d
Weston Sandfort <sandfortw@gmail.com>
2023-02-02 13:32:55 -0700

    Add test for link to author books on author show page

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -26,4 +26,13 @@ describe 'author show page' do
     visit "/authors/#{author.id}"
     expect(page).to have_content("Book count: 2")
   end
+
+  it 'should have a link to its book page' do
+    author = Author.create!(name: 'Shakespeare', year_born: 1564, alive: false)
+    book = Book.create!(title:'Outliers', author_id: author.id, year_written: 2008, fiction: false)
+    book1 = Book.create!(title:'Something book', author_id: author.id, year_written: 2008, fiction: false)
+    visit "/authors/#{author.id}"
+    click_link('Books')
+    expect(current_path).to eq("/authors/#{author.id}/books")
+  end
 end
