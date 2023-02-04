79c342eb9dc69285df9ff072585dc7ce57eeb7f7
Weston Sandfort <sandfortw@gmail.com>
2023-02-04 12:15:17 -0700

    Updates book index to include Update Book link, changes book edit page to have prefilled fields

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,16 +29,16 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update({
+    @book = Book.find(params[:id])
+    @book.update({
                   title: params[:book][:title],
                   author_id: params[:book][:author_id].to_i,
                   year_written: params[:book][:year_written].to_i,
                   fiction: ActiveModel::Type::Boolean.new.cast(params[:book][:fiction]),
                   updated_at: DateTime.now
                 })
-    book.save
-    redirect_to "/books/#{book.id}"
+    @book.save
+    redirect_to "/books/#{@book.id}"
   end
 
   def destroy
--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -1,14 +1,3 @@
-# frozen_string_literal: true
-
-# [ ] done
-
-# User Story 4, Child Show
-
-# As a visitor
-# When I visit '/child_table_name/:id'
-# Then I see the child with that id including the child's attributes
-# (data from each column that is on the child table)
-
 require 'rails_helper'
 
 describe 'books show' do
